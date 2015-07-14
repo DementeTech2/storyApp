e34b5f6bf5bc876072bdaa7932c19b6c72d5e5e3
Gerson Yarce Franco <gersonyarce@useitweb.com>
2015-07-13 20:04:32 -0500

    Story App

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name storyAppApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the storyAppApp
+ */
+angular.module('storyAppApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name storyAppApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the storyAppApp
+ */
+angular.module('storyAppApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
